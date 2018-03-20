706b84e6b184fcf7b81f08585cbd4f6e2740f50e
Muhammed Emre Konar <muhammed.emre.konar@zalando.de>
2018-03-20 16:51:58 +0100

    #1 google analytics stuff removed

--- src/ui/index.jsx
+++ src/ui/index.jsx
@@ -7,14 +7,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Tracker from 'react-ga';
 import Zourney from './components/Zourney';
 // console.log('Zourney', Zourney);
-import { APP_NAME, GA_ID } from 'settings';
+import { APP_NAME } from 'settings';
 import store from 'redux/store';
 /* eslint-enable */
 
-Tracker.initialize(GA_ID);
 
 const rootElement = document.querySelector(`#${APP_NAME}`);
 if (rootElement) {
